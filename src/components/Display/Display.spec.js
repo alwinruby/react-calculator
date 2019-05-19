9fdd889422e84289581d2413004f4eadd339f887
asolanky <alwinsolanky@gmail.com>
2019-05-19 23:00:43 +0100

    Display tests

--- src/components/Display/Display.spec.js
+++ src/components/Display/Display.spec.js
@@ -6,8 +6,14 @@ describe('Display', () => {
   let wrapper;
   // beforeEach(() => wrapper = shallow(<Display />));
   beforeEach(() => wrapper = shallow(<Display displayValue={''} />));
-  
+
   it('should render a <div />', () => {
     expect(wrapper.find('div').length).toEqual(1);
   });
+
+  it('renders the value of displayValue', () => {
+    wrapper.setProps({ displayValue: 'test' });
+    expect(wrapper.text()).toEqual('test');
+  });
+
 });
