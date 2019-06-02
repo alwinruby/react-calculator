45341f6555db20437cdd5f93a606489d2b72f1dc
asolanky <alwinsolanky@gmail.com>
2019-06-02 07:50:15 +0100

    tidy up

--- src/components/Calculator/Calculator.spec.js
+++ src/components/Calculator/Calculator.spec.js
@@ -1,44 +1,8 @@
 import Display from '../Display/Display';
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import Calculator from './Calculator';
 import Keypad from '../Keypad/Keypad';
-import { mount } from 'enzyme';
-import { shallow } from 'enzyme';
-
-// describe('Calculator', () => {
-//   let wrapper;
-//   beforeEach(() => wrapper = shallow(<Calculator />));
-//
-//   it('should render a <div />', () => {
-//     expect(wrapper.find('div').length).toEqual(1);
-//   });
-//
-//   it('should render the Display Component', () => {
-//
-//     expect(wrapper.containsMatchingElement(
-//       <Display displayValue={wrapper.instance().state.displayValue} />
-//     )).toEqual(true);
-//
-//   });
-//
-//   it('should render the Display and Keypad Components', () => {
-//     expect(wrapper.containsAllMatchingElements([
-//       <Display displayValue={wrapper.instance().state.displayValue} />,
-//       <Keypad
-//         callOperator={wrapper.instance().callOperator}
-//         numbers={wrapper.instance().state.numbers}
-//         operators={wrapper.instance().state.operators}
-//         setOperator={wrapper.instance().setOperator}
-//         updateDisplay={wrapper.instance().updateDisplay}
-//       />
-//     ])).toEqual(true);
-//   });
-//
-//   it('should render correctly', () => expect(wrapper).toMatchSnapshot());
-//
-// });
-
 
 describe('mounted Calculator', () => {
   let wrapper;
