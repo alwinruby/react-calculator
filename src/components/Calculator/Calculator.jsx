4c77b82e1cfa971e4660a6ad1c5fb13400f2e8a7
asolanky <alwinsolanky@gmail.com>
2019-06-03 19:15:39 +0100

    Updates not working

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -5,18 +5,24 @@ import Keypad from '../Keypad/Keypad';
 
 
 class Calculator extends Component {
+  // state = {
+  //   // value to be displayed in <Display />
+  //   displayValue: '0',
+  //   // values to be displayed in number <Keys />
+  //   numbers: ['9', '8', '7', '6', '5', '4', '3', '2', '1', '.', '0','ce'],
+  //   // values to be displayed in operator <Keys />
+  //   operators: ['/', 'x', '-', '+'],
+  //   // operator selected for math operation
+  //   selectedOperator: '',
+  //   // stored value to use for math operation
+  //   storedValue: '',
+  // }
+
   state = {
-    // value to be displayed in <Display />
     displayValue: '0',
-    // values to be displayed in number <Keys />
-    numbers: ['9', '8', '7', '6', '5', '4', '3', '2', '1', '.', '0','ce'],
-    // values to be displayed in operator <Keys />
-    operators: ['/', 'x', '-', '+'],
-    // operator selected for math operation
-    selectedOperator: '',
-    // stored value to use for math operation
-    storedValue: '',
   }
+
+  updateDisplay = value => this.setState({ displayValue: value });
 
   callOperator = () => {
     console.log('call operation');
@@ -26,15 +32,28 @@ class Calculator extends Component {
     console.log('set operation');
   }
 
-  updateDisplay = () => {
-    console.log('update display');
-  }
-
-  // render = () => {
-  //   return (
-  //     <div className="calculator-container" />
-  //   );
+  // updateDisplay = () => {
+  //   console.log('update display');
   // }
+  updateDisplay = value => {
+    let { displayValue } = this.state;
+
+    // prevent multiple occurences of '.'
+    if (value === '.' && displayValue.includes('.')) value = '';
+
+    if (value === 'ce') {
+      // deletes last char in displayValue
+      displayValue = displayValue.substr(0, displayValue.length - 1);
+      // set displayValue to '0' if displayValue is empty string
+      if (displayValue === '') displayValue = '0';
+    } else {
+      // replace displayValue with value if displayValue equal to '0'
+      // else concatenate displayValue and value
+      displayValue === '0' ? displayValue = value : displayValue += value;
+    }
+
+    this.setState({ displayValue });
+  }
   render = () => {
   // unpack the component state by using Object Destructuring
     const { displayValue, numbers, operators } = this.state;
